cf4b5fc570b5289353ac629395818cfd908306ba
Rajesh kumar <rajeshkumar.d190@gmail.com>
2023-08-11 12:19:13 +0530

    Adding Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly x = new RWOnly();
+
+        x.name = "Rajesh";
+        System.out.println(x.name);
+
+        x.setName("Michael");
+        System.out.println(x.getName());
+
+    }
 }
